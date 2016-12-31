7a13b96a43a8f3f52938941fac4ef808f6d41594
Shrey Desai <shreydesai@me.com>
2016-12-31 09:59:38 -0800

    added probing hash

--- hashing/probing.py
+++ hashing/probing.py
@@ -9,26 +9,37 @@ class Empty:
 class HashTable:
 
     DEFAULT_SIZE = 10
+    LOAD_LIMIT = 0.5
 
     def __init__(self):
         self.table = [None] * self.DEFAULT_SIZE
         self.keys = 0
 
     def resize(self):
-        pass
+        table = [None] * (len(self.table) * 2)
+        for i in range(len(self.table)):
+            if self.table[i] is not None and \
+                type(self.table[i]) is not Empty:
+                index = hash(i) % len(table)
+                while table[index] is not None and \
+                    type(table[index]) is not Empty:
+                    index = (index + 1) % len(table)
+                table[index] = self.table[i]
+        self.table = table
 
     def insert(self, key):
         self.keys += 1
-        # TODO: resize method
+        if self.keys / len(self.table) > self.LOAD_LIMIT:
+            self.resize()
         index = hash(key) % len(self.table)
         while self.table[index] is not None and \
             type(self.table[index]) is not Empty:
-            index += 1
+            index = (index + 1) % len(self.table)
         self.table[index] = key
 
     def remove(self, key):
-        index = hash(key) % len(self.table)
         search = 0
+        index = hash(key) % len(self.table)
         while search < len(self.table):
             slot = (index + search) % len(self.table)
             if self.table[slot] is None or \
@@ -42,8 +53,8 @@ class HashTable:
         return False
 
     def search(self, key):
-        index = hash(key) % len(self.table)
         search = 0
+        index = hash(key) % len(self.table)
         while search < len(self.table):
             slot = (index + search) % len(self.table)
             if self.table[slot] is None or \
