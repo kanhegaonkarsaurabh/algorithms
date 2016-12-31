e66c018d1ebfcf3d0533b3e6d8140741dbbf3835
Shrey Desai <shreydesai@me.com>
2016-12-30 23:01:10 -0800

    changed chains to lists

--- hashing/chaining.py
+++ hashing/chaining.py
@@ -3,16 +3,16 @@ class HashTable:
     DEFAULT_SIZE = 10
 
     def __init__(self):
-        self.table = [()] * self.DEFAULT_SIZE
+        self.table = [[] for i in range(self.DEFAULT_SIZE)]
         self.elems = 0
 
     def resize(self):
-        table = [()] * (len(self.table) * 2)
+        new_size = len(self.table) * 2
+        table = [[] for i in range(new_size)]
         for i in range(len(self.table)):
-            elems = list(self.table[i])
-            for elem in elems:
+            for elem in self.table[i]:
                 index = hash(elem) % len(table)
-                table[index] += (elem,)
+                table[index].append(index)
         self.table = table
 
     def insert(self, elem):
@@ -20,13 +20,12 @@ class HashTable:
         if self.elems / len(self.table) > 0.5:
             self.resize()
         index = hash(elem) % len(self.table)
-        self.table[index] += (elem,)
+        self.table[index].append(elem)
 
     def remove(self, elem):
         index = hash(elem) % len(self.table)
         if elem in self.table[index]:
-            self.table[index] = tuple(x for x in \
-                self.table[index] if x != elem)
+            self.table[index].remove(elem)
             self.elems -= 1
 
     def contains(self, elem):
