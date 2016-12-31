ffca9ed053c987d4c0682be10e416830e5654cda
Shrey Desai <shreydesai@me.com>
2016-12-30 23:03:09 -0800

    renamed elems var to keys

--- hashing/chaining.py
+++ hashing/chaining.py
@@ -4,30 +4,30 @@ class HashTable:
 
     def __init__(self):
         self.table = [[] for i in range(self.DEFAULT_SIZE)]
-        self.elems = 0
+        self.keys = 0
 
     def resize(self):
         new_size = len(self.table) * 2
         table = [[] for i in range(new_size)]
         for i in range(len(self.table)):
-            for elem in self.table[i]:
-                index = hash(elem) % len(table)
+            for key in self.table[i]:
+                index = hash(key) % len(table)
                 table[index].append(index)
         self.table = table
 
-    def insert(self, elem):
-        self.elems += 1
-        if self.elems / len(self.table) > 0.5:
+    def insert(self, key):
+        self.keys += 1
+        if self.keys / len(self.table) > 0.5:
             self.resize()
-        index = hash(elem) % len(self.table)
-        self.table[index].append(elem)
+        index = hash(key) % len(self.table)
+        self.table[index].append(key)
 
-    def remove(self, elem):
-        index = hash(elem) % len(self.table)
-        if elem in self.table[index]:
-            self.table[index].remove(elem)
-            self.elems -= 1
+    def remove(self, key):
+        index = hash(key) % len(self.table)
+        if key in self.table[index]:
+            self.table[index].remove(key)
+            self.keys -= 1
 
-    def contains(self, elem):
-        index = hash(elem) % len(self.table)
-        return True if elem in self.table[index] else False
+    def contains(self, key):
+        index = hash(key) % len(self.table)
+        return True if key in self.table[index] else False
